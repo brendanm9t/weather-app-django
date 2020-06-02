f6f992f3be865316555b452b06d192a8f517cf2e
brendan mitton <brendanmitton@gmail.com>
2020-06-02 17:33:54 +0200

    update views.py
    Parsing though Python list for render to homepage

--- lookup/views.py
+++ lookup/views.py
@@ -4,16 +4,18 @@ from django.shortcuts import render
 def home(request):
     import json
     import requests
+    #import datetime
 
     api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=1&API_KEY=DD6F3AF3-415F-446C-BDBE-721221738FE8")
     api = json.loads(api_requests.content)
+    #date = datetime.datetime.strptime(api[0]['DateObserved'], '%Y-%m-%d')
+    #date.strftime('%d-%m-%Y')
 # Python try-except routine
     #try:
         #api = json.loads(api_requests.content)
     #except Exception as e:
         #api = "420-Error"
 
-    # http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=1&API_KEY=DD6F3AF3-415F-446C-BDBE-721221738FE8
     return render(request, 'home.html', {'api': api})
 
 def about(request):
