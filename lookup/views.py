4222623d9ffc513ea6444e50ebce08da7a4280d4
brendan mitton <brendanmitton@gmail.com>
2020-06-02 15:47:16 +0200

    json import to homepage view
    json import to homepage view

--- lookup/views.py
+++ lookup/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    import json
+    # http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=1&API_KEY=DD6F3AF3-415F-446C-BDBE-721221738FE8
     return render(request, 'home.html', {})
 
 def about(request):
